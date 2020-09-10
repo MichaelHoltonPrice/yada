8094bc7aac94b111f1a2041df89dfa7fd074b034
Michael Holton Price <MichaelHoltonPrice@gmail.com>
2020-09-09 22:34:46 -0600

    Updated param_constr2unconstr and param_unconstr2constr to use category vectors rather than matrices

--- R/variable_transforms.R
+++ R/variable_transforms.R
@@ -1,67 +1,73 @@
 #' Transform a vector from a constrained to an unconstrained representation.
-#' The categories and bounds are:
-#' category	 min	max
-#' 0		-Inf	Inf
-#' 1		   0	Inf
-#' 2		  -1	 +1
+#' The categories are:
+#' Category	Description
+#' 0		Unconstrained
+#' 1		Positive
+#' 2		-1 to 1
+#' 3		Positive difference
 #' 
 #' @param param The vector of constrained parameters
-#' @param transfMat The matrix specifying the transform
-#' @return The vector unconstrained parameters, param_bar
+#' @param tfCatVect The vector of transformation categories
+#' @return The vector of unconstrained parameters, param_bar
 #' @export
-param_constr2unconstr <- function(param,transfMat) {
-  cat_vect <- transfMat[,1]
-  ind_other <- (cat_vect < 0) & (2 < cat_vect)
+param_constr2unconstr <- function(param,tfCatVect) {
+  ind_other <- (tfCatVect < 0) & (3 < tfCatVect)
   if(any(ind_other)) {
-    stop('Categories must be between 0 and 2')
+    stop('Categories must be between 0 and 3')
   }
 
   param_bar <- rep(NA,length(param))
-  for(categ in 0:2) {
-    ind <- cat_vect == categ
+  for(categ in 0:3) {
+    ind <- which(tfCatVect == categ)
     if(sum(ind) > 0) {
       if(categ == 0) {
-        param_bar[ind] <- param[transfMat[ind,2]]
+        param_bar[ind] <- param[ind]
       } else if(categ == 1) {
-        param_bar[ind] <- log(param[transfMat[ind,2]])
+        param_bar[ind] <- log(param[ind])
+      } else if(categ == 2) {
+        param_bar[ind] <- gtools::logit((param[ind]+1)/2)
       } else {
-        # categ = 2
-        param_bar[ind] <- gtools::logit((param[transfMat[ind,2]]+1)/2)
+        # categ = 3
+        param_bar[ind] <- log(param[ind] - param[ind-1])
       }
     }
   }
   return(param_bar)
 }
 
-#' Transform a vector from an unconstrained to a unconstrained representation.
-#' The categories and bounds are:
-#' category	 min	max
-#' 0		-Inf	Inf
-#' 1		   0	Inf
-#' 2		  -1	 +1
+#' Transform a vector from an unconstrained to a constrained representation.
+#' The categories are:
+#' Category	Description
+#' 0		Unconstrained
+#' 1		Positive
+#' 2		-1 to 1
+#' 3		Positive difference
 #' 
 #' @param param_bar The vector of unconstrained parameters
-#' @param transfMat The matrix specifying the transform
+#' @param tfCatVect The vector of transformation categories
 #' @return The vector of constrained parameters, param
 #' @export
-param_unconstr2constr <- function(param_bar,transfMat) {
-  cat_vect <- transfMat[,1]
-  ind_other <- (cat_vect < 0) & (2 < cat_vect)
+param_unconstr2constr <- function(param_bar,tfCatVect) {
+  ind_other <- (tfCatVect < 0) & (3 < tfCatVect)
   if(any(ind_other)) {
-    stop('Categories must be between 0 and 2')
+    stop('Categories must be between 0 and 3')
   }
 
   param <- rep(NA,length(param_bar))
-  for(categ in 0:2) {
-    ind <- cat_vect == categ
+  for(categ in 0:3) {
+    ind <- which(tfCatVect == categ)
     if(sum(ind) > 0) {
       if(categ == 0) {
-        param[ind] <- param_bar[transfMat[ind,2]]
+        param[ind] <- param_bar[ind]
       } else if(categ == 1) {
-        param[ind] <- exp(param_bar[transfMat[ind,2]])
+        param[ind] <- exp(param_bar[ind])
+      } else if(categ == 2) {
+        param[ind] <- -1 + 2/(1+exp(-param_bar[ind]))
       } else {
-        # categ = 2
-        param[ind] <- -1 + 2/(1+exp(-param_bar[transfMat[ind,2]]))
+        # categ = 3
+        for(n in ind) {
+          param[n] <- param[n-1] + exp(param_bar[n])
+        }
       }
     }
   }
